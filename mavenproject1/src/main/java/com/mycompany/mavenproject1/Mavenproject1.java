4016f928656a4635531df1cb2628c56465914903
unknown <dchapalimbaquin2018@gmail.com>
2023-10-08 21:57:03 -0500

    se modifico el archivo Mavenproject1 que es la clase main

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alexis Chapal
+ */
+public class Mavenproject1 {
+    public static void main (String [] args){
+        JOptionPane.showMessageDialog(null, "hola mundo");
+    }
+    
+}
